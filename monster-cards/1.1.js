6d6df70bd68d289196a5da4d92c980819b5de21a
travis@localhost <travis@localhost>
2016-11-28 11:03:32 +0000

    built example

---
+++ monster-cards/1.1.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{109:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=109}});
+//# sourceMappingURL=1.1.js.map
